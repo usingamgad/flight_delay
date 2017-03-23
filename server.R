2c8f31e6037cb7f64fa1e346837f48f4b85dbbd8
FelixH <fhoeschle@gmail.com>
2017-03-23 18:11:24 +0000

    smylies changing

--- server.R
+++ server.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(png)
+library(lubridate)
 
 airport_map<-read.csv('L_AIRPORT_ID.csv')
 airlines_map<-read.csv('L_AIRLINE_ID.csv')
@@ -16,7 +17,9 @@ airlines_map<-read.csv('L_AIRLINE_ID.csv')
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$Delay <- renderText({
+ 
+  # Push user input to Python script
+  pythonCallResults <- eventReactive(input$button, {
     
     #map_airlines
     airline<-airlines_map[airlines_map$Description==input$Airline,]$Code
@@ -29,33 +32,25 @@ shinyServer(function(input, output) {
     dayofmonth <- day(input$depDate)
     month <- month(input$depDate)
     #print(arrairport)
-  })
-
-  output$my_image<- renderImage({
-    if (1==1)
-    {return(list(src='green1.png',contentType='image/png',alt='All is well. Your flight will not be delayed.'))}
-    else if (1==2)
-    {return(list(src='green2.png',contentType='image/png',alt='Your flight will be delayed, but no more than 30 minutes.'))}
-    else if (1==3)
-    {return(list(src='yellow3.png',contentType='image/png',alt='Your flight will be more than 30 minutes delayed, but no more than 60 minutes.'))}
-    else if (1==3)
-    {return(list(src='orange4.png',contentType='image/png',alt='Your flight will be significantly delayed - more than one hour, but no more than 90 minutes'))}
-    else if (1==3)
-    {return(list(src='red5.png',contentType='image/png',alt='You flight will be significantly delayed - more than 90 minutes.'))}
-    else
-    {return(NULL)}
-  },deleteFile=FALSE)
-  
-  # Push user input to Python script
-  pythonCallResults <- eventReactive(input$button, {
-    ##command<-paste("python FlightApp_main.py ",input$DayOfWeek,input$ArrTime,airport,airline )
-    command<-paste("python FlightApp_main.py 1 1 1 19805 11298 11433 11")
+    
+    command<-paste("python FlightApp_main.py ",airline,depairport,arrairport,dayofweek,dayofmonth,dayofmonth,month) 
+    ##command<-paste("python FlightApp_main.py 1 1 1 19805 11298 11433 11")
     result<-system(command,intern = TRUE)
-    print(result)
-    paste0("Delay in minutes: ", result)
-  })
+    
+    return(result)
+      })
   
-  output$est <- renderText({
-    pythonCallResults()
-  })
+    output$my_image<-renderImage({
+    if(pythonCallResults()=="['No Delay']")
+    {list(src='yellow3.png')}
+    else 
+    {list(src='orange4.png')}
+    },deleteFile = FALSE)
+  
+  output$est <- renderText({pythonCallResults()})
+  
+ 
+   
+  
+  
   })
